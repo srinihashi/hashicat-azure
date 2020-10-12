d95aaed35e6c1495308b31a666bb06b814b743f0
Your Name <asdf@example.com>
2020-10-12 21:28:35 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
-module "network" {
-  source  = "app.terraform.io/wolfer-training/network/azurerm"
-  version = "3.2.1"
-  resource_group_name = "${var.prefix}-workshop"
-}
+# module "network" {
+#   source  = "app.terraform.io/wolfer-training/network/azurerm"
+#   version = "3.2.1"
+#   resource_group_name = "${var.prefix}-workshop"
+# }
