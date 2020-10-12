6c210e2dd34a9bb41a24cf4a45976b8129d7bf5c
Your Name <asdf@example.com>
2020-10-12 23:14:25 +0000

    yay

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source              = "app.terraform.io/wolfer-training/network/azurerm"
-  version             = "3.2.1"
+  version             = "3.2.2"
   resource_group_name = "${var.prefix}-workshop"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
