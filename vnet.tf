a96201d697894f167d4f26ce7b5bea611a344133
Your Name <asdf@example.com>
2020-10-12 22:05:43 +0000

    stuff

--- vnet.tf
+++ vnet.tf
@@ -4,6 +4,7 @@ module "network" {
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  address_prefix      = "10.0.1.0/24"
 
   tags = {
     environment = "dev"
