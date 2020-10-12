34f5d414ee08584acfd86daa9db637fb1ff15a89
joshwolfer <47542028+joshwolfer@users.noreply.github.com>
2020-10-12 18:03:42 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -5,7 +5,6 @@ module "network" {
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  address_prefix      = var.subnet_prefixes[count.index]
 
   tags = {
     environment = "dev"
