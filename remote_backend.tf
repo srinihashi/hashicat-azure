ffd917bdc84586120b402b14367a2c18453dab1a
Srini <srini+github-api-token.com>
2020-10-13 17:00:31 +0000

    adding remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,25 +1,12 @@
 # If you are in a workshop...
 # Do not delete this file!
 # It's required to complete the Instruqt labs.
-<<<<<<< HEAD
+
 terraform {
   backend "remote" {
     organization = "srini-hashi-azure"
-
-=======
-
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "wolfer-training"
->>>>>>> 6039203939a8b244c77e80224e2a6884148e67f4
     workspaces {
       name = "hashicat-azure"
     }
   }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> 6039203939a8b244c77e80224e2a6884148e67f4
